f541e7f45f52623cb35ff26294365c0d6ae967df
Francisco Arredondo <frarredo@gmail.com>
2019-06-29 21:25:55 -0700

    added some placeholder code for backend routes

---
+++ routes/contacts.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router();
+
+// @route   GET    api/contacts
+// @desc    Get all user contacts
+// @access  Private
+router.get('/', (req, res) => res.send('Get all contacts'));
+
+// @route   POST    api/contacts
+// @desc    Add a  new contact
+// @access  Private
+router.post('/', (req, res) => res.send('Add a new contact'));
+
+// @route   PUT    api/contacts
+// @desc    Update a contact
+// @access  Private
+router.put('/:id', (req, res) => res.send('Update a contact'));
+
+// @route   DELETE    api/contacts
+// @desc    Delete a contact
+// @access  Private
+router.delete('/', (req, res) => res.send('Delete a contact'));
+
+module.exports = router;
